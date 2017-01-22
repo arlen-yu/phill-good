91dca2dfdee57e4bd226dd4ccee592e3c12a98d2
Arlen Yu <arlen.yu6@gmail.com>
2017-01-22 02:34:26 -0500

    update

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def home():
 def tally():
 	location = request.form['address']
 
+	#keyPoint: [name, lat, lng, description HTML]
+
 	# crime info
 	hr = 23
 	location_range = al.get_coord_range(location)
@@ -20,26 +22,33 @@ def tally():
 	danger = al.danger_decile(al.get_score(location_range, 23))
 	violent_map = al.get_violent(crime_map)
 
+
 	# police info
 	coords = al.get_coords(location)
 	police_result = ps.get_closest_location(coords[0], coords[1], "police_stations")
 	closest_police = police_result[0][4]
 	telephone = police_result[0][5]
+	police_html = "<strong>Nearest Police Station</strong><br>Address: " + closest_police + "<br> Phone: " + str(telephone)
+	police_kp = [closest_police, police_result[1], police_result[2], police_html]
 
 	#correctional info
 	correctional_result = ps.get_closest_location(coords[0], coords[1], "correctional")
 	closest_correctional = correctional_result[0][3]
 	c_telephone = correctional_result[0][8]
+	correctional_html = "<strong>Nearest Correctional Facility</strong><br>Address: " + closest_correctional + "<br> Phone: " + str(c_telephone)
+	
 
 	#healthy screening info
 	health_result = ps.get_closest_location(coords[0], coords[1], "heart_healthy")
 	closest_health = health_result[0][5]
 	h_telephone = health_result[0][12]
+	health_html = "<strong>Nearest Health Screening Facility</strong><br>Address: " + closest_health + "<br> Phone: " +str(h_telephone)
 
 	#farmers market
 	farmers_result = ps.get_closest_location(coords[0], coords[1], "farmers_market")
 	closest_market = farmers_result[0][5]
 	h_description = farmers_result[0][6]
+	
 
 	#chinese food
 	chinese_result = ps.get_closest_location(coords[0], coords[1], "chinese")
